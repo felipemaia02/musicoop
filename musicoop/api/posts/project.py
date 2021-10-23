20457c69bc27a82fcdc6b8bf62df6eef7d50c272
felipemaia02 <felipeoliveiramaia03@gmail.com>
2021-10-22 21:13:32 -0300

    Adicionando o endpoint dos comentários

--- musicoop/api/posts/project.py
+++ musicoop/api/posts/project.py
@@ -114,6 +114,11 @@ async def new_project(
         "file_size": file_size,
         "user":1
     })
+    if file.content_type != "audio/mp3":
+        raise HTTPException(
+        status_code=status.HTTP_406_NOT_ACCEPTABLE,
+        detail="Arquivo não é valido, apenas mp3!"
+    )
     if save_file is False:
         raise HTTPException(
         status_code=status.HTTP_406_NOT_ACCEPTABLE,
