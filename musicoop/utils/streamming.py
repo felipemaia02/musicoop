7b407ee2f2b254a2c8adf15ded436a87784a418d
felipemaia02 <felipeoliveiramaia03@gmail.com>
2021-10-29 09:01:17 -0300

    BugFix File_like

--- musicoop/utils/streamming.py
+++ musicoop/utils/streamming.py
@@ -15,13 +15,15 @@ def iterfile(file: str, start: int, end: int, path_type: str) -> None:
         Raises
         ------
     """
-    try:
-        with open("musicoop/static/" + path_type + "/" + file, mode="rb") as file_like:
-            file_like.seek(start)
-            reader = partial(file_like.read, end - start)
-            file_iterator = iter(reader, bytes())
-            for byte in file_iterator:
-                yield byte
 
-    finally:
-        file_like.close()
+    file_like = None
+    with open("musicoop/static/" + path_type + "/" + file, mode="rb") as file_like:
+        file_like.seek(start)
+        reader = partial(file_like.read, end - start)
+        file_iterator = iter(reader, bytes())
+        for byte in file_iterator:
+            if byte:
+                yield byte
+            else:
+                file_like.close()
+                break
