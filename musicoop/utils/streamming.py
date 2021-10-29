cb1868a59d53e5ca18843c3d7970b56530ea017e
felipemaia02 <felipeoliveiramaia03@gmail.com>
2021-10-28 21:14:01 -0300

    Corrigindo bug do streamming

--- musicoop/utils/streamming.py
+++ musicoop/utils/streamming.py
@@ -2,10 +2,9 @@
     Modulo
 """
 from functools import partial
-CHUNK_SIZE = 1024*1024
 
 
-def iterfile(file: str, start: int, end: int, size:int, path_type: str) -> None:
+def iterfile(file: str, start: int, end: int, path_type: str) -> None:
     """
         Description
         -----------
@@ -20,8 +19,6 @@ def iterfile(file: str, start: int, end: int, size:int, path_type: str) -> None:
         with open("musicoop/static/" + path_type + "/" + file, mode="rb") as file_like:
             file_like.seek(start)
             reader = partial(file_like.read, end - start)
-            if size < CHUNK_SIZE:
-                reader = partial(file_like.read)
             file_iterator = iter(reader, bytes())
             for byte in file_iterator:
                 yield byte
